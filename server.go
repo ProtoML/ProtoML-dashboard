1ec7f93da35fc3f485e2429366c2729face5e0f0
Bharath Santosh <bsant99@gmail.com>
2013-10-30 19:07:57 -0400

    [dashboard] Converted to semantic ui.

--- server.go
+++ server.go
@@ -75,7 +75,7 @@ func save(w http.ResponseWriter, r *http.Request) {
 	MAIN
 **/
 func main() {
-	port := ":80"
+	port := ":8000"
 	http.HandleFunc("/static/", static)
 	http.HandleFunc("/api", api);
 	http.HandleFunc("/save", save);
--- static/javascript/protoml.js
+++ static/javascript/protoml.js
@@ -1,11 +1,19 @@
 $(document).ready(function() {
     paper = new Raphael("raphael",2000, 2000);
-    var myWorker = new Worker("static/worker.js");
+    var myWorker = new Worker("static/javascript/worker.js");
     myWorker.onmessage = function(e) {
         console.log("Worker said: " + e.data);
     }
     events.initialize();
+
+    menuModal(".ui.modal.newtransform",".item.addtransform");
+    menuModal(".ui.modal.searchtransform",".item.searchtransform");
 });
+
+menuModal = function(selector,eventSelector) {
+    $(selector).modal('setting', 'transition', 'fade down');
+    $(selector).modal('attach events', eventSelector, 'show');
+}
 
 createTransform = function() {
     var html =  $('#transform')[0].innerHTML;
